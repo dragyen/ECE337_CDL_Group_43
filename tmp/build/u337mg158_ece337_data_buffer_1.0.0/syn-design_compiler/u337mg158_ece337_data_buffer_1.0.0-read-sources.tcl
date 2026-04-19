c37012ff1e7d7cda254825635191e62c9180e301
337mg158 <337mg158@ececomp5.ecn.purdue.edu>
2026-04-19 14:09:47 -0400

    untrack tmp files

--- tmp/build/u337mg158_ece337_data_buffer_1.0.0/syn-design_compiler/u337mg158_ece337_data_buffer_1.0.0-read-sources.tcl
+++
@@ -1,6 +0,0 @@
-# Auto-generated project tcl file to read all sources
-
-set search_path [concat [list .] $search_path]
-source  src/u337mg158_ece337_synfiles_0/detect-reset-logic.tcl
-analyze -format sverilog -define { } -work work src/u337mg158_ece337_data_buffer_1.0.0/source/data_buffer.sv
-source  src/u337mg158_ece337_data_buffer_1.0.0/scripts/syn_data_buffer.tcl
--- tmp/build/u337mg158_ece337_usb_rx_tx_db_1.0.0/lint-verilator/src/u337mg158_ece337_nrzi_decoder_1.0.0/source/nrzi_decoder.sv
+++
@@ -1,8 +0,0 @@
-module nrzi_decoder(
-    input logic edge_det,
-    output logic decoded_data
-);
-
-assign decoded_data = ~edge_det;
-
-endmodule
